833963378804bc1820f43aea6233b3f27030ae3b
Akzhol <akzhol_b@bk.ru>
2018-03-16 01:41:54 +0600

    VideoUrl check if have http protocol,then not append  SERVER_URL to videoSrc

--- src/components/VideoLectureComponent.js
+++ src/components/VideoLectureComponent.js
@@ -1,6 +1,6 @@
-import React, { Component, PropTypes } from 'react'
+import React, {Component, PropTypes} from 'react'
 import 'video-react/dist/video-react.css'
-import { Player } from 'video-react'
+import {Player} from 'video-react'
 
 
 class VideoLecture extends Component {
@@ -17,24 +17,28 @@ class VideoLecture extends Component {
     }
 
     render() {
-    	let { title, description, cover, videoSrc } = this.props
+        let {title, description, cover, videoSrc} = this.props
+        if (!videoSrc.startsWith('http')) {
+            videoSrc = 'http://smartkitap.avsoft.kz' + videoSrc
+        }
+
         return (
-        	<div>
-	            <div className="video-body__content__player">
-	        		<Player 
-	        			playsInline
-	        			poster={'http://smartkitap.avsoft.kz' + cover}
-	        			src={'http://smartkitap.avsoft.kz' + videoSrc}
-	        			className="video-player-style"
-	        		/>
-	        	</div>
-	    		<div className="video-body__description">
-	            	<h2>{title}</h2>
-	            	<p>		
-	            		{description}
-	            	</p>
-	            </div>
-	        </div>
+            <div>
+                <div className="video-body__content__player">
+                    <Player
+                        playsInline
+                        poster={'http://smartkitap.avsoft.kz' + cover}
+                        src={videoSrc}
+                        className="video-player-style"
+                    />
+                </div>
+                <div className="video-body__description">
+                    <h2>{title}</h2>
+                    <p>
+                        {description}
+                    </p>
+                </div>
+            </div>
         )
     }
 }
